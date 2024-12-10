a4cd22e676b81013779988402601a3ee93cdd401
eaksquad <36047591+eaksquad@users.noreply.github.com>
2024-12-10 14:33:40 -0600

    ijoj

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       outDir: 'dist',
+      emptyOutDir: true,
       rollupOptions: {
         output: {
           manualChunks(id) {
